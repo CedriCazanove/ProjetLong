ee070ab3dbec332fbefc02809fe7444fb15d0574
CedriCazanove <cedricazanove@hotmail.fr>
2023-02-21 14:53:15 +0100

    Acquisition done

---
+++ app/src/main/java/com/example/bodysway/Mesure.java
@@ -0,0 +1,38 @@
+package com.example.bodysway;
+
+public class Mesure implements Comparable<Object> {
+
+    private float x;
+
+    private float z;
+
+    public Mesure() {
+    }
+
+    public Mesure(float x, float z) {
+        this.x = x;
+        this.z = z;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Mesure f = (Mesure) o;
+        return (int)(this.x - f.getX());
+    }
+}
